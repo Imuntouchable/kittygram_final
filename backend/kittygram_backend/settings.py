e512c76822b295d38af4b32f6a37d10a36580708
Александр <sasatoguzov@gmail.com>
2024-04-03 19:25:27 +0000

    Act

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.164.156', '127.0.0.1', 'localhost', 'teryllosite.sytes.net'] 
+ALLOWED_HOSTS = ['84.201.164.156', '127.0.0.1', 'localhost', 'teryllosite.sytes.net']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -98,7 +98,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
